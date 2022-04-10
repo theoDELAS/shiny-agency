bb262a9c1ec2c8c162cba5bd7dce72487def846a
Théo Delas <theo.delas@gmail.com>
2022-04-10 19:23:33 +0200

    Routing

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./pages/Home/index";
+import Home from "./pages/Home";
+import Survey from "./pages/Survey";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import Error from "./components/Error";
+import Results from "./pages/Results";
+import Freelances from "./pages/Freelances";
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Router>
+            <Header />
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/survey/:questionNumber">
+                    <Survey />
+                </Route>
+                <Route path="/results">
+                    <Results />
+                </Route>
+                <Route path="/freelances">
+                    <Freelances />
+                </Route>
+                <Route>
+                    <Error />
+                </Route>
+            </Switch>
+        </Router>
     </React.StrictMode>,
     document.getElementById("root")
 );
