dab1b6503799a0248a2bc253a319bea2b5c94171
Théo Delas <theo.delas@gmail.com>
2022-04-15 19:59:36 +0200

    ADD typage with PropTypes

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+
+function Card({ label, title, picture }) {
+    return (
+        <div style={{ display: "flex", flexDirection: "column", padding: 15 }}>
+            <span>{label}</span>
+            <img src={picture} alt="freelance" height={80} width={80} />
+            <span>{title}</span>
+        </div>
+    );
+}
+
+Card.propTypes = {
+    label: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+};
+
+Card.defaultProps = {
+    label: "",
+    title: "",
+    picture: "https://picsum.photos/200/300",
+};
+
+export default Card;
