1ddf223522f79aba57057beac1aa574f1ea06a0f
Théo Delas <theo.delas@gmail.com>
2022-04-18 19:02:25 +0200

    Use context (theme, survey)

--- src/index.jsx
+++ src/index.jsx
@@ -1,42 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Survey from "./pages/Survey";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Header from "./components/Header";
-import Error from "./components/Error";
 import Results from "./pages/Results";
 import Freelances from "./pages/Freelances";
-import { createGlobalStyle } from "styled-components";
-
-const GlobalStyle = createGlobalStyle`
-    div {
-        font-family: 'Trebuchet MS', Helvetica, sans-serif;
-    }
-`;
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Error from "./components/Error";
+import GlobalStyle from "./utils/styles/GlobalStyle";
+import { ThemeProvider, SurveyProvider } from "./utils/context";
 
 ReactDOM.render(
     <React.StrictMode>
         <Router>
-            <GlobalStyle />
-            <Header />
-            <Switch>
-                <Route exact path="/">
-                    <Home />
-                </Route>
-                <Route path="/survey/:questionNumber">
-                    <Survey />
-                </Route>
-                <Route path="/profils">
-                    <Freelances />
-                </Route>
-                <Route path="/results">
-                    <Results />
-                </Route>
-                <Route>
-                    <Error />
-                </Route>
-            </Switch>
+            <ThemeProvider>
+                <SurveyProvider>
+                    <GlobalStyle />
+                    <Header />
+                    <Switch>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                        <Route path="/survey/:questionNumber">
+                            <Survey />
+                        </Route>
+                        <Route path="/results">
+                            <Results />
+                        </Route>
+                        <Route path="/profils">
+                            <Freelances />
+                        </Route>
+                        <Route>
+                            <Error />
+                        </Route>
+                    </Switch>
+                    <Footer />
+                </SurveyProvider>
+            </ThemeProvider>
         </Router>
     </React.StrictMode>,
     document.getElementById("root")
--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,9 +1,10 @@
-import { useEffect, useState, useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { Loader } from "../../utils/styles/Atoms";
 import colors from "../../utils/styles/colors";
+import { Loader } from "../../utils/styles/Atoms";
+import { SurveyContext } from "../../utils/context";
 
 const SurveyContainer = styled.div`
     display: flex;
@@ -30,6 +31,31 @@ const LinkWrapper = styled.div`
     }
 `;
 
+const ReplyBox = styled.button`
+    border: none;
+    height: 100px;
+    width: 300px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: ${colors.backgroundLight};
+    border-radius: 30px;
+    cursor: pointer;
+    box-shadow: ${(props) =>
+        props.isSelected ? `0px 0px 0px 2px ${colors.primary} inset` : "none"};
+    &:first-child {
+        margin-right: 15px;
+    }
+    &:last-of-type {
+        margin-left: 15px;
+    }
+`;
+
+const ReplyWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
 function Survey() {
     const { questionNumber } = useParams();
     const questionNumberInt = parseInt(questionNumber);
@@ -38,7 +64,12 @@ function Survey() {
     const nextQuestionNumber = questionNumberInt + 1;
     const [surveyData, setSurveyData] = useState({});
     const [isDataLoading, setDataLoading] = useState(false);
+    const { answers, saveAnswers } = useContext(SurveyContext);
     const [error, setError] = useState(false);
+
+    function saveReply(answer) {
+        saveAnswers({ [questionNumber]: answer });
+    }
 
     useEffect(() => {
         async function fetchSurvey() {
@@ -47,8 +78,8 @@ function Survey() {
                 const response = await fetch(`http://localhost:8000/survey`);
                 const { surveyData } = await response.json();
                 setSurveyData(surveyData);
-            } catch (error) {
-                console.log(error);
+            } catch (err) {
+                console.log(err);
                 setError(true);
             } finally {
                 setDataLoading(false);
@@ -67,8 +98,22 @@ function Survey() {
             {isDataLoading ? (
                 <Loader />
             ) : (
-                <QuestionContent>{surveyData[questionNumber]} </QuestionContent>
+                <QuestionContent>{surveyData[questionNumber]}</QuestionContent>
             )}
+            <ReplyWrapper>
+                <ReplyBox
+                    onClick={() => saveReply(true)}
+                    isSelected={answers[questionNumber] === true}
+                >
+                    Oui
+                </ReplyBox>
+                <ReplyBox
+                    onClick={() => saveReply(false)}
+                    isSelected={answers[questionNumber] === false}
+                >
+                    Non
+                </ReplyBox>
+            </ReplyWrapper>
             <LinkWrapper>
                 <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
                 {surveyData[questionNumberInt + 1] ? (
